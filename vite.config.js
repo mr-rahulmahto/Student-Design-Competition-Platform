05cf5d185a9a9f31cd94eb47bd0f692350284fcb
Rahul Mahto <rahulmahto2486@gmail.com>
2026-08-19 02:14:00 +0530

    vercel.json

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://student-design-competition-platform.vercel.app/',
         changeOrigin: true,
         secure: false,
       },
